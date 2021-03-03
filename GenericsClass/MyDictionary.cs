3a6d1f519f25b6cb40728d6b2d246ae189416d5b
fatmaGnay <77405136+fatmaGnay@users.noreply.github.com>
2021-03-03 16:16:17 +0300

    Update MyDictionary.cs

--- GenericsClass/MyDictionary.cs
+++ GenericsClass/MyDictionary.cs
@@ -6,8 +6,21 @@ namespace GenericsClass
 {
     class MyDictionary<T> //Generic Class
     {
-           public void Add(T item)
+        T[] items;
+        public MyDictionary()
         {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;
+            items = new T[items.Length+1];
+            for (int i =0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+
+            }
+            items[items.Length - 1] = item;
             Console.WriteLine("Sözcük eklendi.");
         }
     }
