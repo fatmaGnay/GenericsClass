28f9b9ecb82b45cb2d8e84a1eb7a9cb061fb0386
fatmaGnay <77405136+fatmaGnay@users.noreply.github.com>
2021-03-03 15:41:09 +0300

    Proje dosyası ekle.

---
+++ GenericsClass/MyDictionary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsClass
+{
+    class MyDictionary<T> //Generic Class
+    {
+           public void Add(T item)
+        {
+            Console.WriteLine("Sözcük eklendi.");
+        }
+    }
+}
---
+++ GenericsClass/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GenericsClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> sozcukler = new MyDictionary<string>();
+            sozcukler.Add("Fatma");
+        }
+    }
+}
